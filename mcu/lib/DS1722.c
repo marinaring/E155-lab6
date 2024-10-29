5303664ff2f00eebbf7382edd3c29e09003dbe13
Marina Ring <mring@hmc.edu>
2024-10-29 15:54:46 -0700

    fixed bug with CS pin number (don't use PA6), now sending right data package length (2 8 bit packages)

--- mcu/lib/DS1722.c
+++ mcu/lib/DS1722.c
@@ -11,43 +11,31 @@
 void configureTemp(int resolution) {
   
   // send signal that we are changing the configuration register
-  digitalWrite(PB6, 1);
+  digitalWrite(CS, 1);
   spiSendReceive(0x80);
-  digitalWrite(PB6, 0);
 
   // send new configuration
   int default_config = 0b11100000;
   int new_config = (resolution << 1) | default_config; // concatenate default configuration with resolution chosen
-
-  digitalWrite(PB6, 1);
+  
   spiSendReceive(new_config);
-  digitalWrite(PB6, 0);
+  digitalWrite(CS, 0);
 
 };
 
 
 float readTemp(void) {
-  digitalWrite(PB6, 1);
+  digitalWrite(CS, 1);
   spiSendReceive(0x01);
-  digitalWrite(PB6, 0);
-
-  delay_millis(TIM15, 1);
-
-  digitalWrite(PB6, 1);
   char temp_lsb = spiSendReceive(0x00);
-  digitalWrite(PB6, 0);
+  digitalWrite(CS, 0);
 
   delay_millis(TIM15, 1);
 
-  digitalWrite(PB6, 1);
+  digitalWrite(CS, 1);
   spiSendReceive(0x02);
-  digitalWrite(PB6, 0);
-
-  delay_millis(TIM15, 1);
-
-  digitalWrite(PB6, 1);
   char temp_msb = spiSendReceive(0x00);
-  digitalWrite(PB6, 0);
+  digitalWrite(CS, 0);
 
   delay_millis(TIM15, 1);
 
